ea0247e52c4774bc5446ad5e4b5a0110f93fe078
Robbe De Ghein <robbe.de.ghein@skyline.be>
2024-11-06 12:57:28 +0100

    add (non-)swarmable counts

--- Element Count Per Agent_1/Element Count Per Agent_1.cs
+++ Element Count Per Agent_1/Element Count Per Agent_1.cs
@@ -67,17 +67,22 @@ namespace Element_Count_Per_Agent_1
         private string _subscriptionID;
 
         private GetDataMinerInfoResponseMessage[] _agentInfos;
-        private Dictionary<ElementID, int> _elementToHost = new Dictionary<ElementID, int>();
+        private Dictionary<ElementID, (int, bool)> _elementToHost = new Dictionary<ElementID, (int, bool)>();
 
         private static readonly GQIStringColumn _stateColumn = new GQIStringColumn("Agent State");
-        private static readonly GQIIntColumn _countColumn = new GQIIntColumn("Element Count");
+        private static readonly GQIIntColumn _nonSwarmableElementCountColumn = new GQIIntColumn("Non-Swarmable Element Count");
+        private static readonly GQIIntColumn _swarmableElementCountColumn = new GQIIntColumn("Swarmable Element Count");
+        private static readonly GQIIntColumn _totalElementCountColumn = new GQIIntColumn("Total Element Count");
 
         private readonly GQIColumn[] _columns = new GQIColumn[]
         {
             new GQIIntColumn("Agent ID"),
             new GQIStringColumn("Agent Name"),
             _stateColumn,
-            _countColumn,
+
+            _nonSwarmableElementCountColumn,
+            _swarmableElementCountColumn,
+            _totalElementCountColumn,
         };
 
         public GQIColumn[] GetColumns() => _columns;
@@ -105,24 +110,29 @@ namespace Element_Count_Per_Agent_1
             {
                 _elementToHost = elementInfos.ToDictionary(
                     elementInfo => elementInfo.ToElementID(),
-                    elementInfo => elementInfo.HostingAgentID);
+                    elementInfo => (elementInfo.HostingAgentID, elementInfo.IsSwarmable));
             }
 
-            var agentToElementsCount= _agentInfos
+            var agentWithElementsCounts = _agentInfos
                 .GroupJoin
                     (
                         elementInfos.GroupBy(elementInfo => elementInfo.HostingAgentID),
                         agentInfo => agentInfo.ID,
                         elementInfoGroup => elementInfoGroup.Key,
-                        (agentInfo, elementInfoGroups) => (agentInfo, elementInfoGroups.SelectMany(x => x).Count())
+                        (agentInfo, elementInfoGroups) =>
+                        {
+                            var nonSwarmableCount = elementInfoGroups.Sum(group => group.Count(elementInfo => !elementInfo.IsSwarmable));
+                            var swarmableCount = elementInfoGroups.Sum(group => group.Count(elementInfo => elementInfo.IsSwarmable));
+                            var totalCount = nonSwarmableCount + swarmableCount;
+                            return (agentInfo, nonSwarmableCount, swarmableCount, totalCount);
+                        }
                     )
-                .ToDictionary(kvp => kvp.Item1, kvp => kvp.Item2);
+                .ToArray();
 
             var rows = new List<GQIRow>(elementInfos.Length);
-            foreach (var kvp in agentToElementsCount)
+            foreach (var entry in agentWithElementsCounts)
             {
-                var agentInfo = kvp.Key;
-                var elementCount = kvp.Value;
+                var agentInfo = entry.agentInfo;
 
                 rows.Add(new GQIRow(
                     agentInfo.ID.ToString(),
@@ -131,7 +141,10 @@ namespace Element_Count_Per_Agent_1
                         new GQICell() { Value = agentInfo.ID, DisplayValue = agentInfo.ID.ToString() },
                         new GQICell() { Value = agentInfo.AgentName, DisplayValue = agentInfo.AgentName },
                         new GQICell() { Value = agentInfo.ConnectionState.ToString(), DisplayValue = agentInfo.ConnectionState.ToString() },
-                        new GQICell() { Value = elementCount, DisplayValue = elementCount.ToString() },
+                        
+                        new GQICell() { Value = entry.nonSwarmableCount, DisplayValue = entry.nonSwarmableCount.ToString() },
+                        new GQICell() { Value = entry.swarmableCount, DisplayValue = entry.swarmableCount.ToString() },
+                        new GQICell() { Value = entry.totalCount, DisplayValue = entry.totalCount.ToString() },
                     }));
             }
 
@@ -186,7 +199,10 @@ namespace Element_Count_Per_Agent_1
             lock (_elementToHost)
             {
                 int oldHost = -1;
-                _elementToHost.TryGetValue(elementId, out oldHost);
+                if(_elementToHost.TryGetValue(elementId, out var oldHostAndSwarmable))
+                {
+                    oldHost = oldHostAndSwarmable.Item1;
+                }
 
                 if (oldHost == newHost)
                     newHost = -1; // not really a new host
@@ -201,22 +217,29 @@ namespace Element_Count_Per_Agent_1
                 {
                     // new or swarmed element
                     _logger.Information($"Updating host '{elementInfo.Name}' ({elementId}) to {newHost}");
-                    _elementToHost[elementId] = newHost;
+                    _elementToHost[elementId] = (newHost, elementInfo.IsSwarmable);
 
                     // Recalculate count for new host
-                    var newCountNewHost = _elementToHost.Count(kvp => kvp.Value == newHost);
-                    _logger.Information($"Updating new host {newHost} count to {newCountNewHost}");
-                    updater.UpdateCell(newHost.ToString(), _countColumn, newCountNewHost);
+                    UpdateHostCounts(updater, newHost);
                 }
 
                 if (oldHost > 0)
                 {
                     // Recalculate count for old host
-                    var newCountOldHost = _elementToHost.Count(kvp => kvp.Value == oldHost);
-                    _logger.Information($"Updating old host {oldHost} count to {newCountOldHost}");
-                    updater.UpdateCell(oldHost.ToString(), _countColumn, newCountOldHost);
+                    UpdateHostCounts(updater, oldHost);
                 }
             }
+        }
+
+        private void UpdateHostCounts(IGQIUpdater updater, int hostID)
+        {
+            var totalElements = _elementToHost.Where(element => element.Value.Item1 == hostID).ToArray();
+            var nonSwarmableElementsCount = totalElements.Count(element => !element.Value.Item2);
+            var swarmableElementsCount = totalElements.Length - nonSwarmableElementsCount;
+
+            updater.UpdateCell(hostID.ToString(), _nonSwarmableElementCountColumn, nonSwarmableElementsCount);
+            updater.UpdateCell(hostID.ToString(), _swarmableElementCountColumn, swarmableElementsCount);
+            updater.UpdateCell(hostID.ToString(), _totalElementCountColumn, totalElements.Length);
         }
 
         private void OnDataMinerInfoEvent(IGQIUpdater updater, DataMinerInfoEvent dataMinerInfo)
